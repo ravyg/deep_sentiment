6ef81638c93e86a44a83fe34d8de08b868dd5b8f
Ravish Gupta <rgupt3888@gmail.com>
2016-11-16 14:44:48 -0600

    Adding Defaults Twitter Authentication File

---
+++ defaultauth.py
@@ -0,0 +1,11 @@
+#NOTE change the name of this file to auth.py after you change the keys. To turn the code on!
+class TwitterAuth:
+	# Go to http://dev.twitter.com and create an app. 
+	# The consumer key and secret will be generated for you after
+    consumer_key="<YOURKEYGOESHERE>"
+    consumer_secret="<YOURSECRET>"
+
+	# After the step above, you will be redirected to your app's page.
+	# Create an access token under the the "Your access token" section
+    access_token="<YOURTOKEN>"
+    access_token_secret="<TOKENSECRET>"
